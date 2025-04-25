3018b22cfe39bfb5fda84d80fed454fef03c7bfa
othman meziane <you@example.com>
2025-04-25 20:55:45 +0200

    bene

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-""" The task_wait_n function was created with the specified prototype."""
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
 import asyncio
 from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ spawn task_wait_random n times with the specified max_delay"""
-            listounette = []
-                for i in range(n):
-                    listounette.append(task_wait_random(max_delay))
-                                sor = await asyncio.gather(*listounette)
-                                    return sorted(sor)
+    """
+    Let's execute multiple tasks at the same time with async
+    """
+    delays = []
+    tasks = []
+    for number_tasks in range(n):
+        tasks.append(task_wait_random(max_delay))
+    for task in asyncio.as_completed(tasks):
+        # as_completed order tasks by end of execution (fastest first)
+        delay = await task
+        delays.append(delay)
+    return delays 
