497144c21a30998f636b611a43531cec29a2a86f
Pulse Car Location <contact@pulsecarlocation.fr>
2025-04-29 19:55:18 +0200

    final

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+"""
+Write a coroutine called async_generator that takes no arguments.
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10. Use the random module.
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+
+    """
+    Generator that yields 10 random numbers between 0 and 10,
+    waiting 1 second between each yield.
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
